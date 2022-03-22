0ee974f0090e70b3ca01cc62630ba1e9d6cde322
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-22 17:19:12 +0100

    terzo esercizio

--- snack3/script.js
+++ snack3/script.js
@@ -1,1 +1,32 @@
-console.log('js ok')
+console.log('js ok');
+
+
+// Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+
+const userWord = prompt('Inserisci una parola');
+
+const parolaRovesciata = reverseWord(userWord);
+console.log(parolaRovesciata);
+
+
+
+
+
+// funzioni---------------------------------------------------------------------------
+/**
+ * Given a string, return a new string with the same letters in reverse order
+ * @param word - the word to be reversed
+ * @returns Nothing is being returned.
+ */
+function reverseWord(word) {
+    let parolaRovesciata = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        parolaRovesciata += word[i];
+    }
+    return parolaRovesciata;
+}
+
+
+
+
+
